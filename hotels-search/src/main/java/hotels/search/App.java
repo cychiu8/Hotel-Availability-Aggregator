a2ec311bf702c05ea0b1125933a1ab0ae230801f
cychiu <cychiu8@gmail.com>
2024-05-11 01:52:29 +0000

    feat: start spring boot application

--- hotels-search/src/main/java/hotels/search/App.java
+++ hotels-search/src/main/java/hotels/search/App.java
@@ -1,13 +1,26 @@
 package hotels.search;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
-public class App 
+
+ @RestController
+ @SpringBootApplication
+ public class App 
 {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World! Congratulations, you have successfully started your Spring Boot application";
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
