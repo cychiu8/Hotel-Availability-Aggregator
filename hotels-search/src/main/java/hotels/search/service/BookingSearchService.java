d0295e30c08ff9527f67f00a59c5d58b650f9981
cychiu <cychiu8@gmail.com>
2024-05-15 15:48:11 +0900

    fix: the situation when the number of result is zero

--- hotels-search/src/main/java/hotels/search/service/BookingSearchService.java
+++ hotels-search/src/main/java/hotels/search/service/BookingSearchService.java
@@ -24,7 +24,9 @@ public class BookingSearchService extends SearchAbstractService {
     private final SearchResultService searchResultService;
 
 
-    public BookingSearchService(DestinationMappingService destinationMappingService, SearchConditionService searchConditionService, SearchResultService searchResultService) {
+    public BookingSearchService(DestinationMappingService destinationMappingService,
+            SearchConditionService searchConditionService,
+            SearchResultService searchResultService) {
         this.destinationMappingService = destinationMappingService;
         this.searchConditionService = searchConditionService;
         this.searchResultService = searchResultService;
@@ -35,53 +37,47 @@ public class BookingSearchService extends SearchAbstractService {
     public List<SearchResult> getAllSearchResult() {
         List<SearchCondition> conditions = searchConditionService.getAllSearchConditions();
         List<SearchResult> results = new ArrayList<>();
-        for(SearchCondition condition : conditions){
+        for (SearchCondition condition : conditions) {
             SearchResult result = getSearchResultAndSave(condition);
             results.add(result);
         }
-        
+
         return results;
     }
 
 
     @Override
-    public SearchResult getSearchResultAndSave(SearchCondition search){
+    public SearchResult getSearchResultAndSave(SearchCondition search) {
 
         String destinationId = destinationMappingService.getBookingDestinationId(search.getDest());
-        
-        UriComponentsBuilder builder = UriComponentsBuilder.newInstance()
-            .scheme("https")
-            .host("www.booking.com")
-            .path("/searchresults.zh-tw.html")
-            // .queryParam("ss", "%E9%9D%92%E6%A3%AE%E8%BB%8A%E7%AB%99%EF%BC%88Aomori+Station%EF%BC%89%2C+%E9%9D%92%E6%A3%AE%2C+%E9%9D%92%E6%A3%AE%E7%B8%A3%2C+%E6%97%A5%E6%9C%AC")
-            // .queryParam("map", "1")
-            // .queryParam("efdco", "1")
-            // .queryParam("label", "gen173nr-1FCAEoggI46AdIM1gEaHWIAQGYATC4AQfIAQzYAQHoAQH4AQuIAgGoAgO4AuXl-7EGwAIB0gIkZjk2YzU4MWMtZWQ1MC00YWQ3LWJmZTctY2YyNmE0NTEwZGI12AIG4AIB")
-            // .queryParam("sid", "7d982f20d955da37b5ed6b046464fddd")
-            // .queryParam("aid", "304142")
-            .queryParam("lang", "zh-tw")
-            .queryParam("sb", "1")
-            .queryParam("src_elem", "sb")
-            .queryParam("src", "index")
-            .queryParam("dest_id", destinationId)
-            .queryParam("dest_type", "city")
-            .queryParam("ac_position", "1")
-            .queryParam("ac_click_type", "b")
-            .queryParam("ac_langcode", "xt")
-            .queryParam("ac_suggestion_list_length", "5")
-            .queryParam("search_selected", "true")
-            // .queryParam("search_pageview_id", "5af31ef361410115")
-            // .queryParam("ac_meta", "GhBiN2NkMWYwN2YxMjgwMDA5IAEoATICeHQ6BmFvbW9yaUAASgBQAA%3D%3D")
-            .queryParam("checkin", search.getCheckin())
-            .queryParam("checkout", search.getCheckout())
-            .queryParam("group_adults", search.getGroupAdults())
-            .queryParam("group_children", search.getGroupChildren())
-            .queryParam("no_rooms", search.getNoRooms())
-            ;
+
+        UriComponentsBuilder builder = UriComponentsBuilder.newInstance().scheme("https")
+                .host("www.booking.com").path("/searchresults.zh-tw.html")
+                // .queryParam("ss",
+                // "%E9%9D%92%E6%A3%AE%E8%BB%8A%E7%AB%99%EF%BC%88Aomori+Station%EF%BC%89%2C+%E9%9D%92%E6%A3%AE%2C+%E9%9D%92%E6%A3%AE%E7%B8%A3%2C+%E6%97%A5%E6%9C%AC")
+                // .queryParam("map", "1")
+                // .queryParam("efdco", "1")
+                // .queryParam("label",
+                // "gen173nr-1FCAEoggI46AdIM1gEaHWIAQGYATC4AQfIAQzYAQHoAQH4AQuIAgGoAgO4AuXl-7EGwAIB0gIkZjk2YzU4MWMtZWQ1MC00YWQ3LWJmZTctY2YyNmE0NTEwZGI12AIG4AIB")
+                // .queryParam("sid", "7d982f20d955da37b5ed6b046464fddd")
+                // .queryParam("aid", "304142")
+                .queryParam("lang", "zh-tw").queryParam("sb", "1").queryParam("src_elem", "sb")
+                .queryParam("src", "index").queryParam("dest_id", destinationId)
+                .queryParam("dest_type", "city").queryParam("ac_position", "1")
+                .queryParam("ac_click_type", "b").queryParam("ac_langcode", "xt")
+                .queryParam("ac_suggestion_list_length", "5").queryParam("search_selected", "true")
+                // .queryParam("search_pageview_id", "5af31ef361410115")
+                // .queryParam("ac_meta",
+                // "GhBiN2NkMWYwN2YxMjgwMDA5IAEoATICeHQ6BmFvbW9yaUAASgBQAA%3D%3D")
+                .queryParam("checkin", search.getCheckin())
+                .queryParam("checkout", search.getCheckout())
+                .queryParam("group_adults", search.getGroupAdults())
+                .queryParam("group_children", search.getGroupChildren())
+                .queryParam("no_rooms", search.getNoRooms());
 
         String url = builder.toUriString();
         SearchResult result = new SearchResult();
-        
+
 
         try {
             LocalDateTime startTime = LocalDateTime.now();
@@ -95,56 +91,61 @@ public class BookingSearchService extends SearchAbstractService {
             long startLong = startTime.atZone(zoneId).toInstant().toEpochMilli();
             long endLong = endTime.atZone(zoneId).toInstant().toEpochMilli();
             long responseTime = endLong - startLong;
-            
+
             result.setExecuteTime(startTime);
             result.setResponseTime(responseTime);
             result.setExecuteUrl(url);
 
 
             searchResultService.saveSearchResult(result);
-            
+
         } catch (IOException e) {
             // Handle the exception
             e.printStackTrace();
-            
+
             throw new RuntimeException("An error occurred while extracting the response", e);
         }
-        
+
         return result;
     }
 
     private SearchResult extractResponse(Document doc) {
-
-        // get result count
-        Element countElement = doc.selectFirst("h1");
-        String numberOfResultsStr = countElement.text().replaceAll("[^0-9]", "");
-        int numberOfResults = Integer.parseInt(numberOfResultsStr);
-
-        // get price
-        Elements priceElements = doc.select("[data-testid=price-and-discounted-price]");
-
-        int[] prices = new int[priceElements.size()];
-        for (int i = 0; i < priceElements.size(); i++) {
-            Element priceElement = priceElements.get(i);
-            String priceStr = priceElement.text().replaceAll("[^0-9]", "");
-            prices[i] = Integer.parseInt(priceStr);
-        }
-
-        int minPrice = Arrays.stream(prices).min().orElse(0);
-        int maxPrice = Arrays.stream(prices).max().orElse(0);
-
-        String words = "Number of results: " + numberOfResults + "\n";
-        words += "Min price: " + minPrice + "\n";
-        words += "Max price: " + maxPrice + "\n";
-        words += Arrays.toString(prices);
-
-        System.out.println(words);
-
         SearchResult result = new SearchResult();
+        int numberOfResults = 0;
+        int minPrice = 0;
+        int maxPrice = 0;
+        try {
+            // get result count
+            Element countElement = doc.selectFirst("h1");
+            if (countElement == null) {
+                result.setNumberOfResults(numberOfResults);
+                result.setMinPrice(minPrice);
+                result.setMaxPrice(maxPrice);
+                return result;
+            }
+
+            String numberOfResultsStr = countElement.text().replaceAll("[^0-9]", "");
+            numberOfResults = Integer.parseInt(numberOfResultsStr);
+
+            // get price
+            Elements priceElements = doc.select("[data-testid=price-and-discounted-price]");
+            int[] prices = new int[priceElements.size()];
+            for (int i = 0; i < priceElements.size(); i++) {
+                Element priceElement = priceElements.get(i);
+                String priceStr = priceElement.text().replaceAll("[^0-9]", "");
+                prices[i] = Integer.parseInt(priceStr);
+            }
+
+            minPrice = Arrays.stream(prices).min().orElse(0);
+            maxPrice = Arrays.stream(prices).max().orElse(0);
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error occurred while parsing the HTML", e);
+        }
         result.setNumberOfResults(numberOfResults);
         result.setMinPrice(minPrice);
         result.setMaxPrice(maxPrice);
-        
         return result;
     }
 }
--- hotels-search/src/main/java/hotels/search/service/JalanSearchService.java
+++ hotels-search/src/main/java/hotels/search/service/JalanSearchService.java
@@ -17,13 +17,15 @@ import hotels.search.model.SearchCondition;
 import hotels.search.model.SearchResult;
 
 @Service
-public class JalanSearchService extends SearchAbstractService{
+public class JalanSearchService extends SearchAbstractService {
 
     private final DestinationMappingService destinationMappingService;
     private final SearchConditionService searchConditionService;
     private final SearchResultService searchResultService;
 
-    public JalanSearchService(DestinationMappingService destinationMappingService, SearchConditionService searchConditionService, SearchResultService searchResultService) {    
+    public JalanSearchService(DestinationMappingService destinationMappingService,
+            SearchConditionService searchConditionService,
+            SearchResultService searchResultService) {
         this.destinationMappingService = destinationMappingService;
         this.searchConditionService = searchConditionService;
         this.searchResultService = searchResultService;
@@ -33,50 +35,45 @@ public class JalanSearchService extends SearchAbstractService{
     public List<SearchResult> getAllSearchResult() {
         List<SearchCondition> conditions = searchConditionService.getAllSearchConditions();
         List<SearchResult> results = new ArrayList<>();
-        for(SearchCondition condition : conditions){
+        for (SearchCondition condition : conditions) {
             SearchResult result = getSearchResultAndSave(condition);
             results.add(result);
         }
-        
+
         return results;
     }
 
     @Override
     public SearchResult getSearchResultAndSave(SearchCondition search) {
 
-        // String url = "https://www.jalan.net/020000/LRG_020200/?stayYear=2024&stayMonth=5&stayDay=12&stayCount=1&roomCount=1&adultNum=1&minPrice=0&maxPrice=999999&mealType=&kenCd=020000&lrgCd=020200&rootCd=04&distCd=01&roomCrack=100000&reShFlg=1&mvTabFlg=0&listId=0&screenId=UWW1402";
-        
-        String[] destinationCodes = destinationMappingService.getJalanDestinationCodes(search.getDest());
+        // String url =
+        // "https://www.jalan.net/020000/LRG_020200/?stayYear=2024&stayMonth=5&stayDay=12&stayCount=1&roomCount=1&adultNum=1&minPrice=0&maxPrice=999999&mealType=&kenCd=020000&lrgCd=020200&rootCd=04&distCd=01&roomCrack=100000&reShFlg=1&mvTabFlg=0&listId=0&screenId=UWW1402";
+
+        String[] destinationCodes =
+                destinationMappingService.getJalanDestinationCodes(search.getDest());
         long stayCount = ChronoUnit.DAYS.between(search.getCheckin(), search.getCheckout());
 
 
-        UriComponentsBuilder builder = UriComponentsBuilder.newInstance()
-            .scheme("https")
-            .host("www.jalan.net")
-            .pathSegment(destinationCodes[0], "LRG_" + destinationCodes[1])
-            .path("/")
-            .queryParam("stayYear", search.getCheckin().getYear())
-            .queryParam("stayMonth", search.getCheckin().getMonthValue())
-            .queryParam("stayDay", search.getCheckin().getDayOfMonth())
-            .queryParam("stayCount", String.valueOf(stayCount)) // Assuming a stay count of 1
-            .queryParam("roomCount", search.getNoRooms())
-            .queryParam("adultNum", search.getGroupAdults())
-            // Add other query parameters as needed
-            .queryParam("minPrice", "0")
-            .queryParam("maxPrice", "999999")
-            .queryParam("mealType", "")
-            .queryParam("kenCd", "020000")
-            .queryParam("lrgCd", "020200")
-            .queryParam("rootCd", "04")
-            .queryParam("distCd", "01")
-            .queryParam("roomCrack", "100000")
-            .queryParam("reShFlg", "1")
-            .queryParam("mvTabFlg", "0")
-            .queryParam("listId", "0")
-            .queryParam("screenId", "UWW1402");
+        UriComponentsBuilder builder =
+                UriComponentsBuilder.newInstance().scheme("https").host("www.jalan.net")
+                        .pathSegment(destinationCodes[0], "LRG_" + destinationCodes[1]).path("/")
+                        .queryParam("stayYear", search.getCheckin().getYear())
+                        .queryParam("stayMonth", search.getCheckin().getMonthValue())
+                        .queryParam("stayDay", search.getCheckin().getDayOfMonth())
+                        .queryParam("stayCount", String.valueOf(stayCount)) // Assuming a stay count
+                                                                            // of 1
+                        .queryParam("roomCount", search.getNoRooms())
+                        .queryParam("adultNum", search.getGroupAdults())
+                        // Add other query parameters as needed
+                        .queryParam("minPrice", "0").queryParam("maxPrice", "999999")
+                        .queryParam("mealType", "").queryParam("kenCd", "020000")
+                        .queryParam("lrgCd", "020200").queryParam("rootCd", "04")
+                        .queryParam("distCd", "01").queryParam("roomCrack", "100000")
+                        .queryParam("reShFlg", "1").queryParam("mvTabFlg", "0")
+                        .queryParam("listId", "0").queryParam("screenId", "UWW1402");
         String url = builder.toUriString();
         SearchResult result = new SearchResult();
-        
+
         try {
             LocalDateTime startTime = LocalDateTime.now();
             Document doc = Jsoup.connect(url).get();
@@ -90,7 +87,7 @@ public class JalanSearchService extends SearchAbstractService{
             long startLong = startTime.atZone(zoneId).toInstant().toEpochMilli();
             long endLong = endTime.atZone(zoneId).toInstant().toEpochMilli();
             long responseTime = endLong - startLong;
-            
+
             result.setExecuteTime(startTime);
             result.setResponseTime(responseTime);
             result.setExecuteUrl(url);
@@ -102,35 +99,45 @@ public class JalanSearchService extends SearchAbstractService{
             e.printStackTrace();
             throw new RuntimeException("An error occurred while extracting the response", e);
         }
-        
+
         return result;
     }
 
     private SearchResult extractResponse(Document doc) {
-        // Convert the response to a result entity
-        Elements countElements = doc.select(".jlnpc-listInformation--count");
-        Elements priceElements = doc.select(".p-searchResultItem__lowestPriceValue");
-        int numberOfResults = Integer.parseInt(countElements.text());
-        int[] prices = new int[numberOfResults];
-        for (int i = 0; i < priceElements.size(); i++) {
-            Element priceElement = priceElements.get(i);
-            String priceStr = priceElement.text().replaceAll("[^0-9]", "");
-            prices[i] = Integer.parseInt(priceStr);
-        }
-
-        int minPrice = Arrays.stream(prices).min().orElse(0);
-        int maxPrice = Arrays.stream(prices).max().orElse(0);
-
-        String words = "Number of results: " + numberOfResults + "\n";
-        words += "Min price: " + minPrice + "\n";
-        words += "Max price: " + maxPrice + "\n";
-        words += Arrays.toString(prices);
 
         SearchResult result = new SearchResult();
+        int numberOfResults = 0;
+        int minPrice = 0;
+        int maxPrice = 0;
+
+        try {
+
+            Elements countElements = doc.select(".jlnpc-listInformation--count");
+            if (countElements.isEmpty()) {
+                result.setNumberOfResults(numberOfResults);
+                result.setMinPrice(minPrice);
+                result.setMaxPrice(maxPrice);
+                return result;
+            }
+            numberOfResults = Integer.parseInt(countElements.first().text());
+
+            Elements priceElements = doc.select(".p-searchResultItem__lowestPriceValue");
+            int[] prices = new int[numberOfResults];
+            for (int i = 0; i < priceElements.size(); i++) {
+                Element priceElement = priceElements.get(i);
+                String priceStr = priceElement.text().replaceAll("[^0-9]", "");
+                prices[i] = Integer.parseInt(priceStr);
+            }
+            minPrice = Arrays.stream(prices).min().orElse(0);
+            maxPrice = Arrays.stream(prices).max().orElse(0);
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error occurred while parsing the HTML", e);
+        }
         result.setNumberOfResults(numberOfResults);
         result.setMinPrice(minPrice);
         result.setMaxPrice(maxPrice);
-        
         return result;
     }
 }
