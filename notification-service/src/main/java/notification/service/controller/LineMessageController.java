592bc6d38dc88829550dac850e32aea4a2f64999
cychiu <cychiu8@gmail.com>
2024-05-17 14:54:36 +0900

    feat: recieve line api

--- notification-service/src/main/java/notification/service/controller/LineMessageController.java
+++ notification-service/src/main/java/notification/service/controller/LineMessageController.java
@@ -1,23 +1,33 @@
 package notification.service.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import notification.service.service.LineMessageService;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 public class LineMessageController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LineMessageController.class);
+
     @Autowired
     private LineMessageService lineMessageService;
 
-    @RequestMapping("/line/test")
-    public ResponseEntity<String> sendNotification() {
+    @PostMapping("/line/send-notification")
+    public ResponseEntity<String> sendNotification(@RequestBody String message) {
 
         try {
-            lineMessageService.sendMessage("testțtttt");
+            logger.info("recived message ... ", message);
+            logger.info("sending message ... ");
+            lineMessageService.sendMessage(message);
+            logger.info("message sent successfully");
             return new ResponseEntity<>(null, HttpStatus.OK);
         } catch (Exception e) {
             System.err.println(e.getMessage());
