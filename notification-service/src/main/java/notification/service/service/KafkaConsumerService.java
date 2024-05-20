f2deeeeb414ce8956a0d7b90599e2a05a845bcc1
cychiu <cychiu8@gmail.com>
2024-05-21 08:07:42 +0900

    refactor: remove unused import

--- notification-service/src/main/java/notification/service/service/KafkaConsumerService.java
+++ notification-service/src/main/java/notification/service/service/KafkaConsumerService.java
@@ -3,7 +3,6 @@ package notification.service.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import notification.service.controller.LineMessageController;
